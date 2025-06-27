b9b48c48263d0f4f137da9a1fe97d2425f26888a
Shyamal Varnan <e1397949@u.nus.edu>
2025-06-27 22:33:18 +0800

    Add files via upload

---
+++ AstroCode(Final)/ui/level_selector.py
@@ -0,0 +1,57 @@
+# Level selection logic and UI
+import pygame
+from core.constants import WHITE, BLUE, CYAN, GREEN, WIDTH, HEIGHT # Import necessary constants
+from ui.button import Button
+from levels.free_world import FreeWorld
+free_world = FreeWorld()
+
+class LevelSelector:
+    def __init__(self, menu_font, title_font):
+        self.levels = []
+        self.menu_font = menu_font
+        self.title_font = title_font
+        self.back_button = Button(WIDTH // 2 - 100, HEIGHT - 80, 200, 50, "Back", BLUE, CYAN, self.menu_font)
+        # Create level buttons grid
+        button_size = 60
+        padding = 20
+        start_x = WIDTH // 2 - (2 * button_size + padding)
+        start_y = HEIGHT // 4
+
+        self.free_roam_button = Button(WIDTH // 2 - 100, HEIGHT // 2 + 70, 200, 50, "Free Roam", BLUE, CYAN,
+                                       self.menu_font)
+
+        for i in range(10):
+            row = i // 5
+            col = i % 5
+            x = start_x + col * (button_size + padding)
+            y = start_y + row * (button_size + padding)
+            btn = Button(x, y, button_size, button_size, str(i + 1), BLUE, GREEN, self.menu_font)
+            self.levels.append(btn)
+
+
+
+
+    def draw(self, surface):
+        title_text = self.title_font.render("Select Level", True, CYAN)
+        title_rect = title_text.get_rect(center=(WIDTH // 2, HEIGHT // 6))
+        surface.blit(title_text, title_rect)
+
+        for btn in self.levels:
+            btn.draw(surface)
+
+        self.back_button.draw(surface)
+        self.free_roam_button.draw(surface)
+
+    def handle_click(self, pos, event):
+        if self.free_roam_button.is_clicked(pos, event):
+            return "free_roam"  # We'll treat 0 as the Free Roam state
+
+        for i, btn in enumerate(self.levels):
+            if btn.is_clicked(pos, event):
+                return i + 1
+
+        if self.back_button.is_clicked(pos, event):
+            return "back"
+
+        return None
+
